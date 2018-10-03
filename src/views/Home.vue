2d6ad2965197744ecec487a10f799360655702d3
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2018-10-03 09:40:08 +0200

    Create a new badge for notifications

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -37,7 +37,7 @@ export default {
                   icon: '/img/icons/android-chrome-192x192.png',
                   image: '/img/autumn-forest.png',
                   vibrate: [300, 200, 300],
-                  badge: '/img/icons/apple-touch-icon-120x120.png',
+                  badge: '/img/icons/plint-badge-96x96.png',
                   // actions: [
                   //     { action: 'confirm', title: 'Okay', icon: '/img/icons/android-chrome-192x192.png'},
                   //     { action: 'cancel', title: 'Cancel', icon: '/img/icons/android-chrome-192x192.png'}
