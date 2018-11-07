de7481ab3f2e83a6a334b320260f22f373844c80
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2018-11-07 13:40:20 +0100

    Add push listener to service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,2 +1,24 @@
 // service-worker.js
 console.log('Our own service worker file!');
+
+// Listen to Push
+self.addEventListener('push', (e) => {
+    let data
+    if (e.data) {
+        data = e.data.json()
+    }
+
+    console.log('data for notification', data);
+
+    const options = {
+        body: data.body,
+        icon: '/img/icons/android-chrome-192x192.png',
+        image: '/img/autumn-forest.png',
+        vibrate: [300, 200, 300],
+        badge: '/img/icons/plint-badge-96x96.png',
+    }
+
+    console.log('options passed to Notification', options);
+
+    e.waitUntil(self.registration.showNotification(data.title, options))
+})
