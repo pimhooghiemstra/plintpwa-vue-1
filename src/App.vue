9cf65a17a9ed176fb1d500788edd61c88168a1b3
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2018-11-21 17:09:03 +0100

    Change notification text

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,30 @@
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
         </div>
-        <p id="notification">This demo reflects the code from <a href="https://github.com/pimhooghiemstra/plintpwa-vue-1/releases/tag/v0.2" target="_blank">this repository</a>. The corresponding post will be on our blog shortly.</p>
+        <p v-show="showMessage" id="notification">
+            This demo reflects the code from <a href="https://github.com/pimhooghiemstra/plintpwa-vue-1/releases/tag/v0.2" target="_blank">this repository</a>. The corresponding post will be on our blog shortly.
+            <br>
+            Please clear your application's storage (using the devTools) if you have played around with this demo before.
+            <button type="button" @click="hideMessage">&times;</button>
+        </p>
         <router-view/>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                showMessage: true,
+            };
+        },
+        methods: {
+            hideMessage() {
+                this.showMessage = false
+            },
+        }
+    };
+</script>
 
 <style lang="less">
 #app {
@@ -28,6 +48,7 @@
     }
 }
 p#notification {
+    position: relative;
     width: 300px;
     margin-left: auto;
     margin-right: auto;
@@ -35,6 +56,20 @@ p#notification {
 
     a {
         color: #42b983;
+    }
+
+    button {
+        position: absolute;
+        top: -20px;
+        right: -20px;
+        padding: 0 5px 2px;
+        background: #1da025;
+        color: #fff;
+        font-size: 16px;
+
+        &:hover {
+            cursor: pointer;
+        }
     }
 }
 </style>
