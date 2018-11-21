1d04c61faa2365cc9dbc3e8cbe363b3920021dc8
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2018-11-21 14:33:48 +0100

    Add notification to App regarding versions

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
         </div>
+        <p id="notification">This demo reflects the code from <a href="https://github.com/pimhooghiemstra/plintpwa-vue-1/releases/tag/v0.2" target="_blank">this repository</a>. The corresponding post will be on our blog shortly.</p>
         <router-view/>
     </div>
 </template>
@@ -24,6 +25,16 @@
         &.router-link-exact-active {
             color: #42b983;
         }
+    }
+}
+p#notification {
+    width: 300px;
+    margin-left: auto;
+    margin-right: auto;
+    font-size: 12px;
+
+    a {
+        color: #42b983;
     }
 }
 </style>
