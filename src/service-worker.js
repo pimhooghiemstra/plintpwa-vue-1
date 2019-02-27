03f41afdada5c5013c21d61d6285bdcd106be4f1
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2019-02-27 17:35:20 +0100

    Add offline capability using registerNavigationRoute

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,6 +9,11 @@ if (workbox) {
     // be imported using importScripts (as is workbox itself) and we can 
     // precache this. This is all we need for precaching
     workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+    // Make sure to return a specific response for all navigation requests.
+    // Since we have a SPA here, this should be index.html always.
+    // https://stackoverflow.com/questions/49963982/vue-router-history-mode-with-pwa-in-offline-mode
+    workbox.routing.registerNavigationRoute('/index.html')
 }
 
 // This code listens for the user's confirmation to update the app.
