b7f1e0143ceb54de752525005072aec3e36787f4
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2019-02-27 14:24:02 +0100

    Add precaching of manifest

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,6 +4,11 @@ console.log('*** Our own service worker! ***');
 if (workbox) {
     // adjust log level for displaying workbox logs
     workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
+
+    // apply precaching. In the built version, the precacheManifest will
+    // be imported using importScripts (as is workbox itself) and we can 
+    // precache this. This is all we need for precaching
+    workbox.precaching.precacheAndRoute(self.__precacheManifest);
 }
 
 // Listen to Push
