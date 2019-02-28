2f103b6e5cee81ed9a67a280b63c2548b6a85e24
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2019-02-28 11:23:22 +0100

    Turn registration of routes off

--- src/service-worker.js
+++ src/service-worker.js
@@ -16,35 +16,35 @@ if (workbox) {
     // Setup cache strategy for Google Fonts. They consist of two parts, a static one
     // coming from fonts.gstatic.com (strategy CacheFirst) and a more ferquently updated on
     // from fonts.googleapis.com. Hence, split in two registerroutes
-    workbox.routing.registerRoute(
-        /^https:\/\/fonts\.googleapis\.com/,
-        new workbox.strategies.StaleWhileRevalidate({
-            cacheName: 'google-fonts-stylesheets',
-        })
-    )
+    // workbox.routing.registerRoute(
+    //     /^https:\/\/fonts\.googleapis\.com/,
+    //     new workbox.strategies.StaleWhileRevalidate({
+    //         cacheName: 'google-fonts-stylesheets',
+    //     })
+    // )
 
-    workbox.routing.registerRoute(
-        /^https:\/\/fonts\.gstatic\.com/,
-        new workbox.strategies.CacheFirst({
-            cacheName: 'google-fonts-webfonts',
-            plugins: [
-                new workbox.cacheableResponse.Plugin({
-                    statuses: [0, 200],
-                }),
-                new workbox.expiration.Plugin({
-                    maxAgeSeconds: 60 * 60 * 24 * 365,
-                    maxEntries: 30,
-                }),
-            ],
-        })
-    )
+    // workbox.routing.registerRoute(
+    //     /^https:\/\/fonts\.gstatic\.com/,
+    //     new workbox.strategies.CacheFirst({
+    //         cacheName: 'google-fonts-webfonts',
+    //         plugins: [
+    //             new workbox.cacheableResponse.Plugin({
+    //                 statuses: [0, 200],
+    //             }),
+    //             new workbox.expiration.Plugin({
+    //                 maxAgeSeconds: 60 * 60 * 24 * 365,
+    //                 maxEntries: 30,
+    //             }),
+    //         ],
+    //     })
+    // )
 
-    workbox.routing.registerRoute(
-        /^https:\/\/stackpath\.bootstrapcdn\.com/,
-        new workbox.strategies.StaleWhileRevalidate({
-            cacheName: 'fontawesome',
-        })
-    );
+    // workbox.routing.registerRoute(
+    //     /^https:\/\/stackpath\.bootstrapcdn\.com/,
+    //     new workbox.strategies.StaleWhileRevalidate({
+    //         cacheName: 'fontawesome',
+    //     })
+    // );
 }
 
 // This code listens for the user's confirmation to update the app.
