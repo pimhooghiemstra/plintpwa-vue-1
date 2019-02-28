df70b2c1760102c6d70ec957a00631d97f48f20a
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2019-02-28 11:02:49 +0100

    Register routes and apply caching strategies

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,6 +12,39 @@ if (workbox) {
     // Since we have a SPA here, this should be index.html always.
     // https://stackoverflow.com/questions/49963982/vue-router-history-mode-with-pwa-in-offline-mode
     workbox.routing.registerNavigationRoute('/index.html')
+
+    // Setup cache strategy for Google Fonts. They consist of two parts, a static one
+    // coming from fonts.gstatic.com (strategy CacheFirst) and a more ferquently updated on
+    // from fonts.googleapis.com. Hence, split in two registerroutes
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'google-fonts-stylesheets',
+        })
+    )
+
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com/,
+        new workbox.strategies.CacheFirst({
+            cacheName: 'google-fonts-webfonts',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                    maxEntries: 30,
+                }),
+            ],
+        })
+    )
+
+    workbox.routing.registerRoute(
+        /^https:\/\/stackpath\.bootstrapcdn\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'fontawesome',
+        })
+    );
 }
 
 // This code listens for the user's confirmation to update the app.
