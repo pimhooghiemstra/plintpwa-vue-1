b00247fbe445c79a2f4947f05b0aea4db6cfb890
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2021-02-11 15:14:02 +0100

    Updated VueCLI from v3 to v4, fix bug due to update in sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,7 +1,8 @@
 // custom service-worker.js
 if (workbox) {
     // adjust log level for displaying workbox logs
-    workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
+    // Doesn't work anymore in v4: https://github.com/GoogleChrome/workbox/issues/2066
+    // workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
 
     // apply precaching. In the built version, the precacheManifest will
     // be imported using importScripts (as is workbox itself) and we can 
